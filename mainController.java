146c91ca28349689bfc433880489e3c9d1856471
Jaken Herman <jch053@shsu.edu>
2016-07-08 05:39:20 +0000

    parseInput added

--- mainController.java
+++ mainController.java
@@ -3,13 +3,13 @@ public class mainController {
     public static void main(String[] args){
 	mainController mc = new mainController();
 	mc.controller();
-	mc.controller_test();
     }
 
     public void controller(){
 	trap system_trap = new trap();
 
-	system_trap.getInput("V1", "Select * from *");
+	system_trap.getInput("V1, Select * from *");
+	system_trap.parseInput();
 	system_trap.pushOutput();
     }
 }
--- trap.java
+++ trap.java
@@ -2,12 +2,23 @@ public class trap {
 
     public String virtualMachine = null;
     public String sqlQuery = null;
+    public String receivedInput = null;
 
-    public void getInput(String virtualMachine, String sqlQuery){
-	this.virtualMachine = virtualMachine;
-	this.sqlQuery = sqlQuery;
+    //receive input
+    public void getInput(String receivedInput){
+	this.receivedInput = receivedInput;
     }
 
+    /*parse the input into which virtual machine
+      sent the command, and what command that vm
+      sent. */
+    public void parseInput(){
+	String[] parsedInput = this.receivedInput.split("\\,", 2);
+        this.virtualMachine = parsedInput[0];
+	this.sqlQuery = parsedInput[1];
+    }
+
+    //print the values of the public variables
     public void pushOutput(){
 	System.out.println("VM: " + this.virtualMachine);
 	System.out.println("Query: " + this.sqlQuery);
