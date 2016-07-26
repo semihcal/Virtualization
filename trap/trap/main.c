be07a138a0f611540c16dba733b3d9fca93c945f
Jaken Herman <Jaken Herman>
2016-07-25 23:47:34 -0500

    attempting honeypot run through java process

---
+++ trap/trap/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  trap
+//
+//  Created by Computer Science on 7/20/16.
+//  Copyright © 2016 jaken herman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
