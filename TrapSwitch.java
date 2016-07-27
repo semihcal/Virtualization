440cb20aec83b2926b4ed3e56693f4e78d23e1b5
jaken herman <jakenherman7@gmail.com>
2016-07-27 00:24:49 -0500

    populatehoneypot method showing null values.

--- TrapSwitch.java
+++ TrapSwitch.java
@@ -52,11 +52,81 @@ public class TrapSwitch {
                 		//fake data
                 		//Jaken connect this
                 		System.out.println("Fake Data");
+                        shuffleData();
                 	}
                 }
              }
         } catch(Exception e){
             System.out.println("Error: " + e);
         }
+    }
+
+    private void shuffleData(){
+        try (BufferedReader br = new BufferedReader(new FileReader("dblog.txt"))){
+            String line;
+            int linecount = 0;
+            
+            while((line = br.readLine()) != null){
+                linecount++;
+            }
+
+            String[] bPolicyNumbers = new String[linecount];
+            String[] businessNames = new String[linecount];
+            String[] taxIDNumbers = new String[linecount];
+            String[] bAccountNumbers = new String[linecount];
+            String[] pPolicyNumbers = new String[linecount];
+            String[] personalNames = new String[linecount];
+            String[] socialSecurities = new String[linecount];
+            String[] pAccountNumbers = new String[linecount];
+
+            int i = 0;
+            while((line = br.readLine()) != null){
+                String[] parsedLog = line.split("\\,", 5);
+
+                if(parsedLog[0].equalsIgnoreCase("B")){
+                    //handle business
+                    bPolicyNumbers[i] = parsedLog[1];
+                    businessNames[i] = parsedLog[2];
+                    taxIDNumbers[i] = parsedLog[3];
+                    bAccountNumbers[i] = parsedLog[4];
+                    i++;
+                }
+                else{
+                    //handle personal
+                    pPolicyNumbers[i] = parsedLog[1];
+                    personalNames[i] = parsedLog[2];
+                    socialSecurities[i] = parsedLog[3];
+                    pAccountNumbers[i] = parsedLog[4];
+                    i++;
+                }
+            }
+            populateHoneypot(bPolicyNumbers, businessNames, taxIDNumbers, bAccountNumbers, pPolicyNumbers,
+                            personalNames, socialSecurities, pAccountNumbers, linecount);
+        } catch(Exception e){
+            System.out.println("Error " + e);
+        }
+    }
+
+    private void populateHoneypot(String[] bPolicyNumbers, String[] businessNames, String[] taxIDNumbers, String[] bAccountNumbers,
+                                    String[] pPolicyNumbers, String[] personalNames, String[] socialSecurities, String[] pAccountNumbers,
+                                    int linecount){
+        //connect to honey
+        //insert into these statements
+        Random rand = new Random();
+        int b = rand.nextInt(linecount) + 0;
+        int c = rand.nextInt(linecount) + 0;
+        int d = rand.nextInt(linecount) + 0;
+        int e = rand.nextInt(linecount) + 0;
+
+
+        //linecount is the maximum and the 0 is our minimum 
+        int j = rand.nextInt(1) + 0;
+        if(j == 0){
+            System.out.println("B"+","+bPolicyNumbers[b]+","+businessNames[c]+","+taxIDNumbers[d]+","+bAccountNumbers[e]);
+        } 
+        else{ 
+            System.out.println("P"+","+pPolicyNumbers[b]+","+personalNames[c]+","+socialSecurities[d]+","+pAccountNumbers[e]);
+        }
+
     }
 }
